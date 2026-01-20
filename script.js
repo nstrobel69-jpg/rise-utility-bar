7da0d493376eea8ce572394d71050b86994bb1ab
nstrobel69-jpg <nstrobel69@gmail.com>
2026-01-20 09:36:55 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function openGlossary() {
+  window.open('glossary.html', 'Glossary', 'width=600,height=500');
+}
+
+function openNotes() {
+  window.open('notes.html', 'Notes', 'width=600,height=500');
+}
